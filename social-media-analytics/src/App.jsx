a9801078931ce24bf7d9e1c113c29c93a0aa0b2d
21rr10 <ranjan.ritesh2112003@gmail.com>
2025-04-02 19:09:25 +0530

    setup

---
+++ social-media-analytics/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-4 bg-blue-500 text-white">
+      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
